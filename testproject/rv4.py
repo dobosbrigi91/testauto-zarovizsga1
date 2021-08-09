ce0c9d0f01b9dc6c57e777eb06feb7298d31175a
Dobos Brigi <dobosbrigi91@gmail.com>
2021-08-09 20:59:50 +0200

    ## 5 Feladat: Kakukktojás - városok - rv4-py

--- testproject/rv4.py
+++ testproject/rv4.py
@@ -3,36 +3,30 @@
 import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-import csv
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import Select
-from random import randint
 
-driver = webdriver.Chrome()
-#driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.get('https://black-moss-0a0440e03.azurestaticapps.net/rv4.html')
 
-time.sleep(5)
+try:
+    time.sleep(5)
 
 
-cities = driver.find_element_by_id('cites')
-cities_list = str(cities.text).replace('"', ',')
-cities_list = cities_list.strip().split(',')
-print(cities_list)
-random = []
-random_cities = driver.find_elements_by_xpath('//ul/li')
+    cities = driver.find_element_by_id('cites')
+    cities_list = str(cities.text).replace('"', ',').split(',')
+    print(cities_list)
+    random = []
+    random_cities = driver.find_elements_by_xpath('//ul/li')
 
-for city in random_cities:
-    if not city.text in cities_list:
-        print(city.text, 'hiányzik')
-    else:
-        print(city.text, 'bennevan')
-
+    for city in random_cities:
+        if not city.text in cities_list:
+            missing_city = city.text
+        else:
+            print(city.text, 'bennevan')
 
 
-#driver.find_element_by_id('missingCity').send_keys(missing_city)
 
-#driver.find_element_by_id('submit').click()
+    driver.find_element_by_id('missingCity').send_keys(missing_city)
+
+    driver.find_element_by_id('submit').click()
+finally:
+    driver.close()
